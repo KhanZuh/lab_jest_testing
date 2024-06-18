0cf9820442cfe8064f6ddba4b3ca0373a60322e0
KhanZuh <khanzuhair96@live.co.uk>
2024-06-18 16:12:13 +0100

    missing tests for subtract and mutliply

--- calculator_testing_start/calculator.test.js
+++ calculator_testing_start/calculator.test.js
@@ -16,25 +16,54 @@ describe('sum', () => {
     expect(actual).toBe(expected);
   });
 
-  test.skip('can add two large positive numbers', () => { 
-    
+  test('can add two large positive numbers', () => { 
+    expected = 200
+    actual = sum(100, 100);
+    expect(actual).toBe(expected);
   });
 
-  test.skip('can add two negative numbers', () => {
-    
+  test('can add two negative numbers', () => {
+    expected = -8
+    actual = sum(-5, -3);
+    expect(actual).toBe(expected);
   });
 
-  test.skip('can add zero', () => {
-    
+  test('can add zero', () => {
+    expected = 5
+    actual = sum(5, 0);
+    expect(actual).toBe(expected);
   });
 
 });
 
 describe('subtract', () => {
+  test("can subtract two positive numbers", () => {
+    expected = 2;
+    actual = subtract(5, 3);
+    expect(actual).toBe(expected);
+
+  })
+
+  test('can subtract a larger number from a smaller number', () => { 
+    expected = -3
+    actual = subtract(2, 5);
+    expect(actual).toBe(expected);
+  });
 
 });
 
 describe('multiply', () => {
+  test("can multiply two positive integers", () =>{
+    expected = 15
+    actual = multiply(5, 3);
+    expect(actual).toBe(expected);
+  });
+
+   test("can multiply a positive and a negative number", () => {
+    expected = -10
+    actual = multiply(5, -2)
+    expect(actual).toBe(expected);
+   })
 
 });
 
